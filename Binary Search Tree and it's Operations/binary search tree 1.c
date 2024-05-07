86cf047bc815f895609d93facb027a269dbbf6d4
Animesh Kumar <animesh14kumar@ce.du.ac.in>
2024-05-08 02:03:17 +0530

    Learning binary search tree and it's operation

---
+++ Binary Search Tree and it's Operations/binary search tree 1.c
@@ -0,0 +1,86 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct TreeNode{
+    int data;
+    struct TreeNode* left;
+    struct TreeNode* right;
+} TreeNode;
+
+typedef struct BinaryTree{
+    TreeNode* root;
+}BinaryTree;
+
+TreeNode* create_tree_node(int data){
+    TreeNode* new_node = (TreeNode*)malloc(sizeof(TreeNode));
+    if(new_node == NULL){
+        printf("Memory not available");
+        exit(1);
+    }
+    new_node->data = data;
+    new_node->left = NULL;
+    new_node->right = NULL;
+    return new_node;
+}
+TreeNode* insert_node(TreeNode* root, int data){
+    if(root == NULL){
+        return create_tree_node(data);
+    }
+    if(data < root->data){
+        root->left = insert_node(root->left,data);
+    }
+    else if(data > root->data){
+        root->right = insert_node(root->right,data);
+    }
+    return root;
+}
+int in_order_traversal(TreeNode* root){
+    if(root != NULL){
+        in_order_traversal(root->left);
+        printf("%d",root->data);
+        in_order_traversal(root->right); 
+    }
+    return 0;
+}
+int post_order_traversal(TreeNode* root){
+    if(root != NULL){
+        post_order_traversal(root->left);
+        post_order_traversal(root->right);
+        printf("%d",root->data); 
+    }
+    return 0;
+}
+int pre_order_traversal(TreeNode* root){
+    if(root != NULL){
+        printf("%d",root->data); 
+        pre_order_traversal(root->left);
+        pre_order_traversal(root->right);
+    }
+    return 0;
+}
+// what can be application of pre_order_traversal in binary search tree
+// we use post order for freeing nodes
+int main(){
+    BinaryTree tree;
+    tree.root = NULL;
+    
+    tree.root = insert_node(tree.root,8);
+    tree.root = insert_node(tree.root,3);
+    tree.root = insert_node(tree.root,10);
+    tree.root = insert_node(tree.root,1);
+    tree.root = insert_node(tree.root,6);
+    tree.root = insert_node(tree.root,14);
+    tree.root = insert_node(tree.root,4);
+    tree.root = insert_node(tree.root,7);
+    tree.root = insert_node(tree.root,13);
+    printf("in_order_traversal\n");
+    in_order_traversal(tree.root);
+    printf("\n");
+    printf("Post_order_traversal\n");
+    post_order_traversal(tree.root);
+    printf("\n");
+    printf("Pre_order_traversal\n");
+    pre_order_traversal(tree.root);
+    printf("\n");
+    return 0;
+}

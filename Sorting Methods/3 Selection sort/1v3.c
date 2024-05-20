bd482ce232c3eaca5c12de7530ca10e217a5b2cf
Animesh Kumar <animesh14kumar@ce.du.ac.in>
2024-05-20 12:37:56 +0530

    updating

---
+++ Sorting Methods/3 Selection sort/1v3.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<limits.h>
+void print_array(int* arr,int n){
+    for(int j = 0; j<n; j++){
+        printf("%d ",arr[j]);      
+    }
+    printf("\n");
+    return;
+}
+void selection_sort(int* arr, int n){
+    int min_index ; //   
+    for(int i = 0; i<n-1;i++){
+        int x = arr[i];
+        //int x = INT_MAX;
+        for(int j=i+1; j<n;j++){
+            if(arr[j] < x){
+                x = arr[j];
+                min_index = j;
+            }
+        }
+        int temp = arr[i];
+        arr[i] = arr[min_index];
+        arr[min_index] = temp;
+        
+    }
+
+    printf("\n");
+    return;
+}
+int main(){
+    int n;
+    printf("Enter No of elements : ");
+   
+    scanf("%d",&n);
+     printf("Enter Value of elements\n");
+    int *arr = (int*)malloc(n * sizeof(int));
+    for(int i = 0; i<n;i++){
+        scanf("%d",&arr[i]);  
+        
+    }
+    
+    selection_sort(arr,n);
+   
+    print_array(arr,n);
+    
+    free(arr);
+    return 0;
+
+}
